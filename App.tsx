9c9f44e6ab4b82dffd3d8b48c26cf7821c386f34
mtking78 <mtking78@gmail.com>
2019-03-10 19:55:47 -0700

    SQLite package added and linked to local db.

--- App.tsx
+++ App.tsx
@@ -10,6 +10,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+// import SQLite from 'react-native-sqlite-storage';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -18,11 +19,49 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-interface Props {}
-export default class App extends Component<Props> {
+var SQLite = require('react-native-sqlite-storage')
+var db = SQLite.openDatabase({
+  name: "TestDatabase",
+  createFromLocation: '~example.db'
+})
+
+// interface Props {}
+export default class App extends Component<{}> {
+
+  public componentDidMount() {
+    SQLite.DEBUG(true);
+    SQLite.enablePromise(true);
+
+    SQLite.openDatabase({
+      name: "TestDatabase",
+      location: "default"
+    }).then((db) => {
+      console.log("Database open!");
+    });
+  }
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      petname: "",
+    };
+
+    db.transaction((tx) => {
+      tx.executeSql('SELECT * FROM pet WHERE owner=?', ['John'], (tx, results) => {
+          var len = results.rows.length;
+          if (len > 0) {
+            var row = results.rows.item(0);
+            this.setState({ petname: row.petname });
+          }
+        });
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
+        <Text>{'John \'s pet is ' + this.state.petname + '!'}</Text>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.tsx</Text>
         <Text style={styles.instructions}>{instructions}</Text>
